808492117660ae4e9e1fd5cab75072cf7d1ac27c
devang-icreativetechnolabs <devang.icreativetechnolabs@gmail.com>
2025-04-01 14:04:44 +0530

    first commit

---
+++ src/components/ConfirmModal.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="modal"
+    tabindex="-1"
+    :class="{ 'show d-block': show }"
+    :style="{ display: show ? 'block' : 'none' }"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header d-flex justify-content-center">
+          <h5 class="modal-title d-flex justify-content-center text-center">
+            Are you sure ?
+          </h5>
+        </div>
+        <div class="modal-body text-center">
+          <p>{{ confirmMessage }}</p>
+        </div>
+        <div class="modal-footer d-flex justify-content-center">
+          <button type="button" class="btn btn-secondary" @click="cancel">
+            No
+          </button>
+          <button type="button" class="btn btn-primary" @click="confirm">
+            Yes
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div v-if="show" class="modal-backdrop fade show"></div>
+</template>
+<script>
+export default {
+  name: "ConfirmModal",
+  props: ["id", "show", "confirmMessage"],
+  emits: ["cancel", "confirm"],
+  methods: {
+    cancel() {
+      this.$emit("cancel");
+    },
+    confirm() {
+      this.$emit("confirm", this.id);
+    },
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <TableView :productList="productList" :categories="categories"> </TableView>
+  </div>
+</template>
+<script>
+import TableView from "@/components/TableView.vue";
+export default {
+  name: "Home",
+  components: {
+    TableView: TableView,
+  },
+  data() {
+    return {
+      url: "https://fakestoreapi.com/products",
+      productList: [],
+      categories: [],
+    };
+  },
+  methods: {
+    async getCategories() {
+      const res = await fetch(`${this.url}/categories`);
+      const data = await res.json();
+      return data;
+    },
+    async getProducts() {
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      const reversData = data.reverse();
+      localStorage.setItem("products", JSON.stringify(reversData));
+      return reversData;
+    },
+  },
+  async created() {
+    if (
+      localStorage.getItem("products") &&
+      JSON.parse(localStorage.getItem("products")).length > 0
+    ) {
+      this.productList = JSON.parse(localStorage.getItem("products"));
+    } else {
+      this.productList = await this.getProducts();
+    }
+    this.categories = await this.getCategories();
+  },
+};
+</script>
